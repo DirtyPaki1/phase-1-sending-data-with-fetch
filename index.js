bd3dfa576ae0dd03360dbe8a5daf09f5d0304039
Usman Qidwai <usmanqidwai21@gmail.com>
2023-10-04 22:41:43 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+      name: name,
+      email: email
+    };
+  
+    let configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let id = data.id;
+        
+        document.body.innerHTML += `<p>New ID: ${id}</p>`;
+
+      })
+      .catch(error => {
+        console.log(error);
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+      });
+  }
+ 
